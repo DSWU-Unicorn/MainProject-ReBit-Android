e5a05974fdf8b3e90a7f133450b20fe0be34b975
allzeroyou <ekdudd119@naver.com>
2023-04-04 23:26:05 +0900

    Feat: 분리해-사진촬영 기능 사용자는 분리수거 쓰레기를 촬영해, 해당 쓰레기의 정확한 분리수거 방법을 확인한다. + 200 포인트, 50 exp 획득

---
+++ app/src/main/java/kr/ac/duksung/rebit/CameraActivity.kt
@@ -0,0 +1,129 @@
+package kr.ac.duksung.rebit
+
+import android.Manifest.permission.CAMERA
+import android.Manifest.permission_group.CAMERA
+import android.content.Context
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.graphics.Bitmap
+import android.net.Uri
+import android.os.Build
+import android.os.Bundle
+import android.provider.MediaStore
+import android.view.LayoutInflater
+import android.widget.Button
+import android.widget.ImageView
+import android.widget.Toast
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.appcompat.app.AlertDialog
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContentProviderCompat.requireContext
+import kr.ac.duksung.rebit.databinding.ActivityCameraBinding
+import java.security.AccessController.getContext
+import java.util.jar.Manifest
+
+
+class CameraActivity : AppCompatActivity() {
+    lateinit var bitmap: Bitmap
+    lateinit var imageView: ImageView
+
+    private lateinit var binding: ActivityCameraBinding
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_camera)
+
+        //객체 생성
+        imageView = findViewById(R.id.imageView)
+        val picBtn: Button = findViewById(R.id.pic_btn)
+
+        //버튼 이벤트
+        picBtn.setOnClickListener {
+            //사진 촬영
+            val intent: Intent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+
+            activityResult.launch(intent)
+
+            binding = ActivityCameraBinding.inflate(layoutInflater)
+            setContentView(binding.root)
+
+            binding.startButton.setOnClickListener {
+
+                // Dialog만들기
+                val mDialogView =
+                    LayoutInflater.from(this).inflate(R.layout.after_model_dialog, null)
+                val mBuilder = AlertDialog.Builder(this)
+                    .setView(mDialogView)
+
+                val mAlertDialog = mBuilder.show()
+
+
+                val okButton = mDialogView.findViewById<Button>(R.id.successButton)
+                okButton.setOnClickListener {
+
+                    Toast.makeText(this, "포인트 획득했습니다!", Toast.LENGTH_SHORT).show()
+                    mAlertDialog.dismiss()
+                }
+
+                val noButton = mDialogView.findViewById<Button>(R.id.AgainButton)
+                noButton.setOnClickListener {
+                    val intent: Intent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+                    activityResult.launch(intent)
+
+                }
+            }
+        }
+
+
+    }//onCreate
+
+
+    //결과 가져오기
+    private val activityResult: ActivityResultLauncher<Intent> = registerForActivityResult(
+        ActivityResultContracts.StartActivityForResult()
+    ) {
+
+        if (it.resultCode == RESULT_OK && it.data != null) {
+            //값 담기
+            val extras = it.data!!.extras
+
+            //bitmap으로 타입 변경
+            bitmap = extras?.get("data") as Bitmap
+
+            //화면에 보여주기
+            imageView.setImageBitmap(bitmap)
+
+
+//            binding=ActivityCameraBinding.inflate(layoutInflater)
+//            setContentView(binding.root)
+//
+//            binding.startButton.setOnClickListener {
+//
+//                // Dialog만들기
+//                val mDialogView = LayoutInflater.from(this).inflate(R.layout.after_model_dialog, null)
+//                val mBuilder = AlertDialog.Builder(this)
+//                    .setView(mDialogView)
+//
+//                val mAlertDialog = mBuilder.show()
+//
+//
+//                val okButton = mDialogView.findViewById<Button>(R.id.successButton)
+//                okButton.setOnClickListener {
+//
+//                    Toast.makeText(this, "포인트 획득했습니다!", Toast.LENGTH_SHORT).show()
+//                    mAlertDialog.dismiss()
+//                }
+//
+//                val noButton = mDialogView.findViewById<Button>(R.id.AgainButton)
+//                noButton.setOnClickListener {
+//                    mAlertDialog.dismiss()
+//                }
+//            }
+
+        }
+    }
+}
+
