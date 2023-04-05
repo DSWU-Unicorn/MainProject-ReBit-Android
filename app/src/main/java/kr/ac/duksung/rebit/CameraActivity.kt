88391a0150bed02c57ee6fc065d46cfee59c6ba8
allzeroyou <ekdudd119@naver.com>
2023-04-05 23:38:53 +0900

    Fix: [분리해] 카메라 버그 수정 - 카메라 실행 권한 묻고, 동의할때 카메라 사용이 가능하게끔. - 사진촬영이 완료되면 data Intent를 이용해 imageView로 보여줌.
    Resolves: #3

--- app/src/main/java/kr/ac/duksung/rebit/CameraActivity.kt
+++ app/src/main/java/kr/ac/duksung/rebit/CameraActivity.kt
@@ -1,17 +1,21 @@
 package kr.ac.duksung.rebit
 
+import android.Manifest
+import android.app.Activity
 import android.content.Intent
+import android.content.pm.PackageManager
 import android.graphics.Bitmap
+import android.os.Build
 import android.os.Bundle
 import android.provider.MediaStore
 import android.view.LayoutInflater
 import android.widget.Button
 import android.widget.ImageView
 import android.widget.Toast
-import androidx.activity.result.ActivityResultLauncher
-import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import kr.ac.duksung.rebit.databinding.ActivityCameraBinding
 
 
@@ -21,6 +25,11 @@ class CameraActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityCameraBinding
 
+    // 권한을 확인할때의 권한 확인을 위함
+    val CAMERA = arrayOf(Manifest.permission.CAMERA)
+
+    // 권한 요청을 위한 권한 자체를 정의
+    val CAMERA_CODE = 98
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -28,18 +37,15 @@ class CameraActivity : AppCompatActivity() {
 
         //객체 생성
         imageView = findViewById(R.id.imageView)
+        // 촬영버튼
         val picBtn: Button = findViewById(R.id.pic_btn)
 
-        //버튼 이벤트
-        picBtn.setOnClickListener {
-            //사진 촬영
-            val intent: Intent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+        binding = ActivityCameraBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
 
-            activityResult.launch(intent)
-
-            binding = ActivityCameraBinding.inflate(layoutInflater)
-            setContentView(binding.root)
-
+        binding.picBtn.setOnClickListener() {
+            CallCamera()
             binding.startButton.setOnClickListener {
 
                 // Dialog만들기
@@ -60,59 +66,65 @@ class CameraActivity : AppCompatActivity() {
 
                 val noButton = mDialogView.findViewById<Button>(R.id.AgainButton)
                 noButton.setOnClickListener {
-                    val intent: Intent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
-                    activityResult.launch(intent)
+                    CallCamera()
+                }
+            } //setOnClickListener
+        }
+    }//onCreate
 
+    fun checkPermission(permissions: Array<out String>): Boolean {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            for (permission in permissions) {
+                if (ContextCompat.checkSelfPermission(
+                        this,
+                        permission
+                    ) != PackageManager.PERMISSION_GRANTED
+                ) {
+                    ActivityCompat.requestPermissions(this, permissions, CAMERA_CODE)
+                    return false;
                 }
             }
         }
 
+        return true;
+    }
 
-    }//onCreate
-
-
-    //결과 가져오기
-    private val activityResult: ActivityResultLauncher<Intent> = registerForActivityResult(
-        ActivityResultContracts.StartActivityForResult()
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
     ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        when (requestCode) {
+            CAMERA_CODE -> {
+                for (grant in grantResults) {
+                    if (grant != PackageManager.PERMISSION_GRANTED) {
+                        Toast.makeText(this, "카메라 권한을 승인해 주세요.", Toast.LENGTH_LONG).show()
+                    }
+                }
+            }
+        }
+    }
 
-        if (it.resultCode == RESULT_OK && it.data != null) {
-            //값 담기
-            val extras = it.data!!.extras
+    fun CallCamera() {
+        if (checkPermission(CAMERA)) {
+            val itt = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+            startActivityForResult(itt, CAMERA_CODE)
+        }
+    }
 
-            //bitmap으로 타입 변경
-            bitmap = extras?.get("data") as Bitmap
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
 
-            //화면에 보여주기
-            imageView.setImageBitmap(bitmap)
-
-
-//            binding=ActivityCameraBinding.inflate(layoutInflater)
-//            setContentView(binding.root)
-//
-//            binding.startButton.setOnClickListener {
-//
-//                // Dialog만들기
-//                val mDialogView = LayoutInflater.from(this).inflate(R.layout.after_model_dialog, null)
-//                val mBuilder = AlertDialog.Builder(this)
-//                    .setView(mDialogView)
-//
-//                val mAlertDialog = mBuilder.show()
-//
-//
-//                val okButton = mDialogView.findViewById<Button>(R.id.successButton)
-//                okButton.setOnClickListener {
-//
-//                    Toast.makeText(this, "포인트 획득했습니다!", Toast.LENGTH_SHORT).show()
-//                    mAlertDialog.dismiss()
-//                }
-//
-//                val noButton = mDialogView.findViewById<Button>(R.id.AgainButton)
-//                noButton.setOnClickListener {
-//                    mAlertDialog.dismiss()
-//                }
-//            }
-
+        if (resultCode == Activity.RESULT_OK) {
+            when (requestCode) {
+                CAMERA_CODE -> {
+                    if (data?.extras?.get("data") != null) {
+                        val img = data?.extras?.get("data") as Bitmap
+                        binding.imageView.setImageBitmap(img)
+                    }
+                }
+            }
         }
     }
 }
