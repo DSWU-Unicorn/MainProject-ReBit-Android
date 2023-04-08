4cc27ec4fc12effafddb5e3c71817b739415d1e2
u__j <20201111@duksung.ac.kr>
2023-04-08 14:35:38 +0900

    Feat: 분리수거 방법 통신 기능 개발

--- app/src/main/java/kr/ac/duksung/rebit/CameraActivity.kt
+++ app/src/main/java/kr/ac/duksung/rebit/CameraActivity.kt
@@ -8,18 +8,34 @@ import android.graphics.Bitmap
 import android.os.Build
 import android.os.Bundle
 import android.provider.MediaStore
+import android.util.Log
 import android.view.LayoutInflater
 import android.widget.Button
 import android.widget.ImageView
+import android.widget.TextView
 import android.widget.Toast
 import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
+import androidx.viewpager.widget.ViewPager
 import kr.ac.duksung.rebit.databinding.ActivityCameraBinding
+import kr.ac.duksung.rebit.network.RetofitClient
+import kr.ac.duksung.rebit.network.RetrofitService
+import kr.ac.duksung.rebit.network.dto.ApiResponse
+import kr.ac.duksung.rebit.network.dto.CardNewsVO
+import kr.ac.duksung.rebit.network.dto.RecycleVO
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+import retrofit2.Retrofit
 
 
 class CameraActivity : AppCompatActivity() {
+    private lateinit var retrofit : Retrofit
+    private lateinit var retrofitService: RetrofitService
+    private lateinit var content : String
+    private lateinit var dataLabel : String
     lateinit var bitmap: Bitmap
     lateinit var imageView: ImageView
 
@@ -34,6 +50,46 @@ class CameraActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_camera)
+
+        // dataLabel 설정
+        dataLabel = "플라스틱"
+
+        // 서버 연결
+        initRetrofit()
+
+        // 분리수거 방법 통신
+        retrofitService.getRecycle(dataLabel)?.enqueue(object :
+            Callback<ApiResponse<RecycleVO>> {
+            override fun onResponse(
+                call: Call<ApiResponse<RecycleVO>>,
+                response: Response<ApiResponse<RecycleVO>>
+            ) {
+                if(response.isSuccessful) {
+                    //정상적으로 통신 성공
+                    val result : ApiResponse<RecycleVO>? = response.body();
+                    val data = result?.getResult();
+
+                    Log.d("Recycle" ,"onresponse 성공: "+ result?.toString() )
+                    Log.d("Recycle", "data : "+ data)
+                    Log.d("Recycle", "content : "+ data?.content)
+                    content = data!!.content
+                    Log.d("content", "content1 : "+ content)
+
+
+                } else {
+                    //통신 실패(응답코드 3xx, 4xx 등)
+                    Log.d("YMC", "onResponse 실패" + response.errorBody().toString())
+                }
+            }
+
+            override fun onFailure(call: Call<ApiResponse<RecycleVO>>, t: Throwable) {
+                //통신 실패(인터넷 끊김, 예외 발생 등 시스템적인 이유)
+                Log.d("YMC", "onFailure 에러: " + t.message.toString());
+            }
+
+        })
+
+
 
         //객체 생성
         imageView = findViewById(R.id.imageView)
@@ -55,6 +111,13 @@ class CameraActivity : AppCompatActivity() {
                     .setView(mDialogView)
 
                 val mAlertDialog = mBuilder.show()
+
+
+                val howto_text = mDialogView.findViewById<TextView>(R.id.howto_text)
+                val title_text = mDialogView.findViewById<TextView>(R.id.title_text)
+                howto_text.text = content
+                title_text.text = dataLabel + "-분리수거 방법"
+                Log.d("content", "content1 : "+ howto_text)
 
 
                 val okButton = mDialogView.findViewById<Button>(R.id.successButton)
@@ -127,5 +190,14 @@ class CameraActivity : AppCompatActivity() {
             }
         }
     }
+
+
+    //서버 연결
+    private fun initRetrofit() {
+        retrofit = RetofitClient.getInstance()
+        retrofitService = retrofit.create(RetrofitService::class.java)
+    }
+
+
 }
 
--- app/src/main/java/kr/ac/duksung/rebit/network/RetrofitService.kt
+++ app/src/main/java/kr/ac/duksung/rebit/network/RetrofitService.kt
@@ -3,6 +3,7 @@ package kr.ac.duksung.rebit.network
 import kr.ac.duksung.rebit.network.dto.ApiResponse
 import kr.ac.duksung.rebit.network.dto.CardNewsVO
 import kr.ac.duksung.rebit.network.dto.RecycleDetailVO
+import kr.ac.duksung.rebit.network.dto.RecycleVO
 import retrofit2.http.GET
 import retrofit2.Call
 import retrofit2.http.Path
@@ -12,7 +13,7 @@ interface RetrofitService {
     fun getCardNews(@Path("date") date : String) : Call<ApiResponse<ArrayList<CardNewsVO>>>
 
     @GET("/recycle/{data-label}")
-    fun getRecycle(@Path("data-label") dataLabel : String) : Call<ApiResponse<String>>
+    fun getRecycle(@Path("data-label") dataLabel : String) : Call<ApiResponse<RecycleVO>>
 
     @GET("/recycle/region/{region}")
     fun getRecycleDetailByRegion(@Path("region") region : String) : Call<ApiResponse<RecycleDetailVO>>
