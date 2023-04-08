bbc77015343f22f5769d0cba77ad8d77a4e01eca
u__j <20201111@duksung.ac.kr>
2023-04-08 16:12:55 +0900

    Feat: Android 현재 위치를 GPS 좌표로 구하기 기능 개발

--- app/src/main/java/kr/ac/duksung/rebit/CameraActivity.kt
+++ app/src/main/java/kr/ac/duksung/rebit/CameraActivity.kt
@@ -2,18 +2,24 @@ package kr.ac.duksung.rebit
 
 import android.Manifest
 import android.app.Activity
+import android.content.Context
 import android.content.Intent
 import android.content.pm.PackageManager
 import android.graphics.Bitmap
+import android.location.Location
+import android.location.LocationManager
 import android.os.Build
 import android.os.Bundle
+import android.os.Looper
 import android.provider.MediaStore
 import android.util.Log
 import android.view.LayoutInflater
+import android.view.WindowManager
 import android.widget.Button
 import android.widget.ImageView
 import android.widget.TextView
 import android.widget.Toast
+import androidx.annotation.RequiresPermission
 import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.ActivityCompat
@@ -29,15 +35,26 @@ import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
 import retrofit2.Retrofit
+import com.google.android.gms.location.*
 
 
 class CameraActivity : AppCompatActivity() {
+    // 통신
     private lateinit var retrofit : Retrofit
     private lateinit var retrofitService: RetrofitService
+    // GPS
+    private var mFusedLocationProviderClient: FusedLocationProviderClient? = null // 현재 위치를 가져오기 위한 변수
+    lateinit var mLastLocation: Location // 위치 값을 가지고 있는 객체
+    internal lateinit var mLocationRequest: LocationRequest // 위치 정보 요청의 매개변수를 저장하는
+    private val REQUEST_PERMISSION_LOCATION = 10
+
     private lateinit var content : String
     private lateinit var dataLabel : String
     lateinit var bitmap: Bitmap
     lateinit var imageView: ImageView
+
+    private lateinit var textView : TextView
+    private lateinit var textView2 : TextView
 
     private lateinit var binding: ActivityCameraBinding
 
@@ -56,6 +73,15 @@ class CameraActivity : AppCompatActivity() {
 
         // 서버 연결
         initRetrofit()
+
+
+        mLocationRequest =  LocationRequest.create().apply {
+
+            priority = LocationRequest.PRIORITY_HIGH_ACCURACY
+
+        }
+
+
 
         // 분리수거 방법 통신
         retrofitService.getRecycle(dataLabel)?.enqueue(object :
@@ -110,14 +136,13 @@ class CameraActivity : AppCompatActivity() {
 
                 val mAlertDialog = mBuilder.show()
 
-
                 val howto_text = mDialogView.findViewById<TextView>(R.id.howto_text)
                 val title_text = mDialogView.findViewById<TextView>(R.id.title_text)
                 howto_text.text = content
                 title_text.text = dataLabel + "-분리수거 방법"
                 Log.d("content", "content1 : "+ howto_text)
 
-
+                /*
                 val okButton = mDialogView.findViewById<Button>(R.id.successButton)
                 okButton.setOnClickListener {
                     postUserPointByRecycle(1L)
@@ -129,11 +154,10 @@ class CameraActivity : AppCompatActivity() {
                      * 포인트 획득 버튼 누르면 Unity로 돌아가게끔 수정 !
                      */
                 }
+                 */
 
-                val noButton = mDialogView.findViewById<Button>(R.id.AgainButton)
-                noButton.setOnClickListener {
-                    CallCamera()
-                }
+
+
             } //setOnClickListener
         }
     }//onCreate
@@ -170,6 +194,18 @@ class CameraActivity : AppCompatActivity() {
                 }
             }
         }
+        // 사용자에게 권한 요청 후 결과에 대한 처리 로직
+        if (requestCode == REQUEST_PERMISSION_LOCATION) {
+            if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                startLocationUpdates()
+
+            } else {
+                Log.d("ttt", "onRequestPermissionsResult() _ 권한 허용 거부")
+                Toast.makeText(this, "권한이 없어 해당 기능을 실행할 수 없습니다.", Toast.LENGTH_SHORT).show()
+            }
+        }
+
+
     }
 
     fun CallCamera() {
@@ -229,6 +265,54 @@ class CameraActivity : AppCompatActivity() {
             }
 
         })
+    }
+
+
+    private fun startLocationUpdates() {
+
+        //FusedLocationProviderClient의 인스턴스를 생성.
+        mFusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(this)
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED
+            && ActivityCompat.checkSelfPermission(this,Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            return
+        }
+        // 기기의 위치에 관한 정기 업데이트를 요청하는 메서드 실행
+        // 지정한 루퍼 스레드(Looper.myLooper())에서 콜백(mLocationCallback)으로 위치 업데이트를 요청
+        mFusedLocationProviderClient!!.requestLocationUpdates(mLocationRequest, mLocationCallback, Looper.myLooper())
+    }
+
+    // 시스템으로 부터 위치 정보를 콜백으로 받음
+    private val mLocationCallback = object : LocationCallback() {
+        override fun onLocationResult(locationResult: LocationResult) {
+            // 시스템에서 받은 location 정보를 onLocationChanged()에 전달
+            locationResult.lastLocation
+            onLocationChanged(locationResult.lastLocation)
+        }
+    }
+
+    // 시스템으로 부터 받은 위치정보를 화면에 갱신해주는 메소드
+    fun onLocationChanged(location: Location) {
+        mLastLocation = location
+        Log.d("latitude", mLastLocation.latitude.toString())
+        textView2.text = "위도 : " + mLastLocation.latitude // 갱신 된 위도
+        textView.text = "경도 : " + mLastLocation.longitude // 갱신 된 경도
+
+    }
+
+    // 위치 권한이 있는지 확인하는 메서드
+    private fun checkPermissionForLocation(context: Context): Boolean {
+        // Android 6.0 Marshmallow 이상에서는 위치 권한에 추가 런타임 권한이 필요
+        return if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            if (context.checkSelfPermission(Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+                true
+            } else {
+                // 권한이 없으므로 권한 요청 알림 보내기
+                ActivityCompat.requestPermissions(this, arrayOf(Manifest.permission.ACCESS_FINE_LOCATION), REQUEST_PERMISSION_LOCATION)
+                false
+            }
+        } else {
+            true
+        }
     }
 
 }
