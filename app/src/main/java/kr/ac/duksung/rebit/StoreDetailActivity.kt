fef4871ec3fead5d9a0875e77834ce616fd632b3
u__j <20201111@duksung.ac.kr>
2023-05-27 15:49:28 +0900

    Fix: merge 오류 수정

--- app/src/main/java/kr/ac/duksung/rebit/StoreDetailActivity.kt
+++ app/src/main/java/kr/ac/duksung/rebit/StoreDetailActivity.kt
@@ -4,6 +4,7 @@ import android.content.Intent
 import android.os.Bundle
 import android.util.Log
 import android.widget.Button
+import android.widget.ImageView
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.bumptech.glide.Glide
@@ -72,7 +73,6 @@ class StoreDetailActivity : AppCompatActivity() {
 //        }
 
 
-=======
 
         //val rand = "${store.id}"
         //val rand = Integer.parseInt(data)
@@ -130,13 +130,13 @@ class StoreDetailActivity : AppCompatActivity() {
             intent.putExtra("store_id", data)         // store_id 값을 review 로 intent 처리
             startActivity(intent)
         }
-        val todoBtn = findViewById<Button>(R.id.todo_btn)
-
-        todoBtn.setOnClickListener {
-            val intent = Intent(this, CreateReviewActivity::class.java) // 리뷰 작성
-            intent.putExtra("store_id", data)
-            startActivity(intent)
-        }
+//        val todoBtn = findViewById<Button>(R.id.todo_btn)
+//
+//        todoBtn.setOnClickListener {
+//            val intent = Intent(this, CreateReviewActivity::class.java) // 리뷰 작성
+//            intent.putExtra("store_id", data)
+//            startActivity(intent)
+//        }
 
         retrofitService.getStoreInfo(rand.toLong()).enqueue(object :
             Callback<ApiResponse<StoreInfoVO>> {
@@ -168,8 +168,8 @@ class StoreDetailActivity : AppCompatActivity() {
                     telText.text = (storeInfoVO.tel).convertNumberToPhoneNumber()
                     // 가게 별점 평균, 가게 리뷰 수
                     try {
-                        starAvgTv.text = (data.reviewList[0].toString())
-                        reviewNumTv.text = (data.reviewList[1].toString())
+                        starAvgTv.text = (storeInfoVO.reviewList[0].toString())
+                        reviewNumTv.text = (storeInfoVO.reviewList[1].toString())
 
                     } catch (e: IndexOutOfBoundsException) {
                         // e.printStackTrace()
